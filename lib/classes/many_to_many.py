0345b2aced21747a8b451da8931cbcefd0154c02
Erin Yant <ghosrider383@gmail.com>
2024-05-21 14:07:57 -0600

    completed with error on bonus item

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,118 @@
+import ipdb
+
 class Coffee:
     def __init__(self, name):
-        self.name = name
-        
+            self._name = name
+       
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if not hasattr(self, '_name') and isinstance(new_name, str) and len(new_name) >= 3:
+            self._name = new_name
+
     def orders(self):
-        pass
+        return [order for order in Order.all if order.coffee == self]
     
     def customers(self):
-        pass
+        return list({order.customer for order in self.orders()})
     
     def num_orders(self):
-        pass
+        return len(self.orders())
     
     def average_price(self):
-        pass
+        prices = [order.price for order in self.orders()]
+        return sum(prices) / len(prices)
+
+    def __repr__(self):
+        return f'<Coffee name={self.name}>'
 
 class Customer:
+    all = []
+
     def __init__(self, name):
         self.name = name
+        Customer.add_new_customer(self)
         
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str) and 1 <= len(new_name) <= 15:
+            self._name = new_name
+
     def orders(self):
-        pass
+        return [order for order in Order.all if order.customer == self]
     
     def coffees(self):
-        pass
+        return list({order.coffee for order in self.orders()})
     
     def create_order(self, coffee, price):
-        pass
+        return Order(coffee=coffee, customer=self, price=price)
     
+    def orders_for_specific_coffee(self, coffee):
+        [order for order in self.orders() if order.coffee == coffee]
+    
+    def total_for_specific_coffee(self, coffee):
+        return sum(order.price for order in self.orders_for_specific_coffee(coffee))
+
+    @classmethod
+    def add_new_customer(cls, new_customer):
+        cls.all.append(new_customer)
+
+    @classmethod
+    def most_aficionado(cls, coffee):
+        if coffee.orders():
+            return max(cls.all, key=lambda customer: customer.total_for_specific_coffee(coffee))
+        else:
+            return None
+
+    def __repr__(self):
+        return f'<Customer name={self.name}>'
+
 class Order:
+    all = []
+
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
-        self.price = price
+        self._price = price
+        Order.add_new_order(self)
+    
+    @classmethod
+    def add_new_order(cls, new_order):
+        cls.all.append(new_order)
+
+    @property
+    def price(self):
+        return self._price
+    
+    @price.setter
+    def price(self, new_price):
+        if isinstance(new_price, float) and 1.0 <= new_price <= 10.0 and not hasattr(self, '_price'):
+            self._price = new_price
+
+    @property
+    def customer(self):
+        return self._customer
+
+    @customer.setter
+    def customer(self, new_customer):
+        if isinstance(new_customer, Customer):
+            self._customer = new_customer
+    
+    @property
+    def coffee(self):
+        return self._coffee
+    
+    @coffee.setter
+    def coffee(self, new_coffee):
+        if isinstance(new_coffee, Coffee):
+            self._coffee = new_coffee
+    
+    def __repr__(self):
+        return f'<Order customer={self.customer.name} coffee={self.coffee.name} price={self.price}>'
--- lib/debug.py
+++ lib/debug.py
@@ -8,4 +8,21 @@ from classes.many_to_many import Coffee
 if __name__ == '__main__':
     print("HELLO! :) let's debug")
 
+    c1 = Customer(name='Emiley')
+    c2 = Customer(name='Connor')
+    c3 = Customer(name='Melana')
+
+    co1 = Coffee(name='Mocha')
+    co2 = Coffee(name='Flat White')
+    co3 = Coffee(name='Iced Coffee')
+
+    o1 = Order(customer=c1, coffee=co1, price=2.0)
+    o2 = Order(customer=c1, coffee=co1, price=2.0)
+    o3 = Order(customer=c1, coffee=co2, price=3.0)
+
+    o4 = Order(customer=c2, coffee=co3, price=4.0)
+
+    # co1.orders() #should return list w/ 2 orders
+    # c1.orders()
+
     ipdb.set_trace()
